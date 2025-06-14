1766d777f4d1bc15962643a653d9bbe048e52f7a
KDlHidden <mohammad13820.mi@gmail.com>
2025-06-14 23:01:00 +0330

    fixing import bugs

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -3,7 +3,7 @@ import { useCabins } from "../cabins/useCabins";
 import { useRecentBookings } from "./useRecentBookings";
 import { useRecentStays } from "./useRecentStays";
 import Spinner from "../../ui/Spinner";
-import Stats from "./stats";
+import Stats from "./Stats";
 import SalesChart from "./SalesChart";
 import DurationChart from "./DurationChart";
 import TodayActivity from "../check-in-out/TodayActivity";
